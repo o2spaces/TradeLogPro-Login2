3147687885d493314bfac029bbf957b79b455d81
o2spaces <148966528+o2spaces@users.noreply.github.com>
2026-04-21 12:18:03 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 
 window.AUTH_GAS_URL = 'https://script.google.com/macros/s/AKfycbxZ_Og81D5wqtijoLkNh3C4W2iFJUc7A6D8j8ylbpoe1416fvNa__m4ifehdkUw5RrqPw/exec';
 
-// Google OAuth 2.0 Client ID (จาก Google Cloud Console)
+// Google OAuth 2.0 Client ID (จาก Google Cloud Console → Credentials)
 window.GOOGLE_CLIENT_ID = '1008136012501-2a6m6ulfb99f5h8jrvc8arpak85t9ckk.apps.googleusercontent.com';
 
 // วิธีใช้:
